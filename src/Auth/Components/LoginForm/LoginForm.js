9446f17de7449e345f4107b31276772a79019300
GulshanSS <sahugulshan686@gmail.com>
2021-11-25 13:33:36 +0530

    Register Fixes

--- src/Auth/Components/LoginForm/LoginForm.js
+++ src/Auth/Components/LoginForm/LoginForm.js
@@ -1,29 +1,69 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import { Container, Row, Form, Button } from "react-bootstrap";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // setLoading(true);
+      const config = {
+        "Content-type": "application/json",
+      };
+      const { data } = await axios.post(
+        "https://ecommerce-robust-api.herokuapp.com/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+      console.log(data);
+      // setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <Container>
         <h3 className="text-center">Login Form</h3>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Row className="d-flex justify-content-center align-items-center">
             <Form.Group className="mb-3" controlId="Email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control
+                type="email"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
           </Row>
           <Row className="d-flex justify-content-center align-items-center">
             <Form.Group className="mb-3" controlId="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
           </Row>
+          <div className="d-flex justify-content-center align-items-center">
+            <Button variant="success" type="submit">
+              Login
+            </Button>
+          </div>
         </Form>
-        <div className="d-flex justify-content-center align-items-center">
-          <Button variant="success">Login</Button>
-        </div>
       </Container>
     </>
   );
--- src/Auth/Components/RegisterForm/RegisterForm.js
+++ src/Auth/Components/RegisterForm/RegisterForm.js
@@ -1,66 +1,190 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./RegisterForm.css";
-
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import { ImCheckmark } from "react-icons/im";
+import { Col, Container, Row, Form, Button, Spinner } from "react-bootstrap";
 
 const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [dob, setDob] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [pass, setPass] = useState("");
+  const [cpass, setCpass] = useState("");
+  const [address, setAddress] = useState("");
+  const [gender, setGender] = useState("M");
+  const [role, setRole] = useState("user");
+  const [message, setMessage] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState("Register");
+  let cpasserr = {};
+  const checkPasswords = (pass, cpass) => {
+    if (pass != cpass) {
+      return { confirmpassword: "Confirm Password is not same as password" };
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      cpasserr = checkPasswords(pass, cpass);
+      const config = {
+        "Content-Type": "application/json",
+      };
+      const { data } = await axios.post(
+        "https://ecommerce-robust-api.herokuapp.com/api/register",
+        {
+          name: name,
+          email: email,
+          mobile: mobile,
+          password: pass,
+          dob: new Date(dob).getTime(),
+          address: address,
+          gender: gender,
+          role: role,
+        },
+        config
+      );
+      setResult(<ImCheckmark />);
+      setLoading(false);
+      setMessage({});
+    } catch (err) {
+      setLoading(false);
+      setResult("Register");
+      setMessage({ ...err.response.data, ...cpasserr });
+    }
+  };
+
   return (
     <>
       <Container>
         <h3 className="text-center">Registration Form</h3>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Row>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Name" />
+                <Form.Control
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name"
+                  isInvalid={!!message.name}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.name}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="Email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email" />
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  isInvalid={!!message.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.email}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="dob">
                 <Form.Label>DOB</Form.Label>
-                <Form.Control type="date" placeholder="Date of Birth" />
+                <Form.Control
+                  type="date"
+                  value={dob}
+                  onChange={(e) => setDob(e.target.value)}
+                  placeholder="Date of Birth"
+                  isInvalid={!!message.dob}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.dob}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="mobileNo">
                 <Form.Label>Mobile No.</Form.Label>
-                <Form.Control type="text" placeholder="Mobile No." />
+                <Form.Control
+                  type="text"
+                  value={mobile}
+                  onChange={(e) => setMobile(e.target.value)}
+                  placeholder="Mobile No."
+                  isInvalid={!!message.mobile}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.mobile}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  value={pass}
+                  onChange={(e) => setPass(e.target.value)}
+                  placeholder="Password"
+                  isInvalid={!!message.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.password && message.password.split("-").length === 1
+                    ? message.password
+                    : message.password &&
+                      message.password.split("-").map((ele, i) => (
+                        <li itemID={i} className="ml-3">
+                          {ele}
+                        </li>
+                      ))}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="confirmPass">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
+                <Form.Control
+                  type="password"
+                  value={cpass}
+                  onChange={(e) => setCpass(e.target.value)}
+                  placeholder="Confirm Password"
+                  isInvalid={!!message.confirmpassword}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.confirmpassword}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="confirmPass">
                 <Form.Label>Address</Form.Label>
-                <Form.Control as="textarea" rows={2} />
+                <Form.Control
+                  as="textarea"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  rows={2}
+                  isInvalid={!!message.address}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {message.address}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col sm="12" md="6">
               <Form.Group className="mb-3" controlId="gender">
                 <Form.Label>Gender</Form.Label>
                 <Form.Check
+                  defaultChecked
                   inline
                   className="ml-3"
                   label="Male"
                   type="radio"
                   name="gendergroup"
+                  value={gender}
+                  onChange={(e) => setGender("M")}
                 />
                 <Form.Check
                   inline
@@ -68,16 +192,21 @@ const RegisterForm = () => {
                   label="Female"
                   type="radio"
                   name="gendergroup"
+                  value={gender}
+                  onChange={(e) => setGender("F")}
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="gender">
                 <Form.Label>Role</Form.Label>
                 <Form.Check
+                  defaultChecked
                   inline
                   className="ml-3"
                   label="User"
                   type="radio"
                   name="rolegroup"
+                  value={role}
+                  onChange={(e) => setRole("user")}
                 />
                 <Form.Check
                   inline
@@ -85,14 +214,22 @@ const RegisterForm = () => {
                   label="Vendor"
                   type="radio"
                   name="rolegroup"
+                  value={role}
+                  onChange={(e) => setRole("vendor")}
                 />
               </Form.Group>
             </Col>
           </Row>
+          <div className="d-flex justify-content-center align-items-center">
+            <Button variant="success" type="submit" style={{ width:"120px", height: "50px" }}>
+              {loading ? (
+                <Spinner animation="border" variant="success" />
+              ) : (
+                result
+              )}
+            </Button>
+          </div>
         </Form>
-        <div className="d-flex justify-content-center align-items-center">
-          <Button variant="success">Register</Button>
-        </div>
       </Container>
     </>
   );
